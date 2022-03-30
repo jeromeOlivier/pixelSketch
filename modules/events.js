f8ca15e7f7fcfdac611b7b1e598aa93806d59076
jeromeOlivier <jerome.olivier@icloud.com>
2022-03-30 15:01:15 -0400

    basic functionality done

--- modules/events.js
+++ modules/events.js
@@ -1,54 +1,92 @@
-// const drawButton = document.querySelector('#draw');
-// const eraseButton = document.querySelector('#erase');
+import { initializeGrid } from './interface.js';
 
+let drawErase = 1; // drawErase 1 to draw and -1 to erase
+let pressure = 1; // three pressure levels 1, 4 and 11
+export let gridSize = 3; // exported to use in drawing grid size
+let invert = true;
+
+// event when draw button is clicked
 function clickDraw() {
   const drawButton = document.querySelector('#draw');
   const eraseButton = document.querySelector('#erase');
   drawButton.addEventListener('click', () => {
     drawButton.className = 'active button locked';
     eraseButton.className = 'idle button locked';
+    drawErase = 1;
   });
 }
-
+// event when erase button is clicked
 function clickErase() {
   const drawButton = document.querySelector('#draw');
   const eraseButton = document.querySelector('#erase');
   eraseButton.addEventListener('click', () => {
     drawButton.className = 'idle button locked';
     eraseButton.className = 'active button locked';
+    drawErase = -1;
   });
 }
-
+// event to adjust drawing or erasing pressure
 function clickPressure() {
   const pressureButton = document.querySelector('#pressure');
   pressureButton.addEventListener('mousedown', () => pressureButton.className = 'active button locked');
   pressureButton.addEventListener('mouseup', () => pressureButton.className = 'idle button locked');
   pressureButton.addEventListener('click', () => {
     if (pressureButton.textContent === 'Pressure: low') {
+      pressure = 4;
       pressureButton.textContent = 'Pressure: med';
     } else if (pressureButton.textContent === 'Pressure: med') {
+      pressure = 11;
       pressureButton.textContent = 'Pressure: high';
     } else {
       pressureButton.textContent = 'Pressure: low';
+      pressure = 1;
     }
   });
 }
-
-function clickPixelSize() {
+// event to adjust the size of the grid
+function clickPixels() {
   const pixelSizeButton = document.querySelector('#size');
   pixelSizeButton.addEventListener('mousedown', () => pixelSizeButton.className = 'active button locked');
   pixelSizeButton.addEventListener('mouseup', () => pixelSizeButton.className = 'idle button locked');
   pixelSizeButton.addEventListener('click', () => {
     if (pixelSizeButton.textContent === 'Pixels: 3x') {
       pixelSizeButton.textContent = 'Pixels: 6x';
+      gridSize = 6;
+      purgeGrid();
+      initializeGrid();
+      draw();
     } else if (pixelSizeButton.textContent === 'Pixels: 6x') {
       pixelSizeButton.textContent = 'Pixels: 9x';
+      gridSize = 9;
+      purgeGrid();
+      initializeGrid();
+      draw();
     } else {
-      pixelSizeButton.textContent = 'Pixels: 3x'
+      pixelSizeButton.textContent = 'Pixels: 3x';
+      gridSize = 3;
+      purgeGrid();
+      initializeGrid();
+      draw();
     }
   });
 }
-
+// event to flip the css
+function clickInvert() {
+  const invertButton = document.querySelector('#invert');
+  const link = document.querySelector('link');
+  invertButton.addEventListener('mousedown', () => invertButton.className = 'active button locked');
+  invertButton.addEventListener('mouseup', () => invertButton.className = 'idle button locked');
+  invertButton.addEventListener('click', () => {
+    if (invert) {
+      link.href = 'styleDark.css';
+      invert = false;
+    } else {
+      link.href = 'styleLight.css';
+      invert = true;
+    }
+  });
+}
+// click and mouse over to draw on the grid
 function draw() {
   const pixels = document.querySelectorAll('.pixel');
   let active = false;
@@ -57,10 +95,19 @@ function draw() {
     pixel.addEventListener('mouseup', () => active = false);
     if (active) {
       let level = Number(pixel.getAttribute('data-level'));
-      level = level === 15 ? 15 : level + 3 > 15 ? 15 : level + 3;
+      if (drawErase === 1) {
+        level = level === 15 ? 15 : level + pressure > 15 ? 15 : level + pressure;
+      } else {
+        level = level === 0 ? 0 : level + drawErase * pressure < 0 ? 0 : level + drawErase * pressure;
+      }
       pixel.setAttribute('data-level', `${ level }`);
     }
   }));
+}
+// remove the grid when changing size to make way for new grid
+function purgeGrid() {
+  const grid = document.querySelector('#grid');
+  grid.innerHTML = '';
 }
 
 export function initializeEvents() {
@@ -68,5 +115,6 @@ export function initializeEvents() {
   clickDraw();
   clickErase();
   clickPressure();
-  clickPixelSize();
+  clickPixels();
+  clickInvert();
 }
--- modules/interface.js
+++ modules/interface.js
@@ -1,20 +1,29 @@
-const menu = document.querySelector('#menu');
-const mesh = document.querySelector('#mesh');
-// width and height rounded to number divisible by four (pixels will be 3 x 3)
-const width = 3 * Math.round((window.innerWidth - (window.innerWidth * .25)) / 3);
-const height = 3 * Math.round((window.innerHeight - (window.innerHeight * .1)) / 3);
-const cols = width / 3;
-const rows = height / 3;
+import { gridSize } from './events.js'
 
-function initializeGrid() {
+const menu = document.querySelector('#menu');
+const grid = document.querySelector('#grid');
+
+// generate grid of squares the user draws on
+export function initializeGrid() {
+  // width and height rounded to number divisible by four (pixels will be 3 x 3)
+  const width = gridSize * Math.round((window.innerWidth - (window.innerWidth * .25)) / gridSize);
+  const height = gridSize * Math.round((window.innerHeight - (window.innerHeight * .1)) / gridSize);
+  const cols = width / gridSize;
+  const rows = height / gridSize;
+
+  console.log('cols' + cols);
+  console.log('rows' + rows);
+
   let i = 0;
   while (i <= cols) {
     const col = document.createElement('div');
     col.setAttribute('data-type', 'col');
-    mesh.appendChild(col);
+    grid.appendChild(col);
     let j = 0;
     while (j <= rows) {
       const pixel = document.createElement('span');
+      pixel.style.width = `${gridSize}px`;
+      pixel.style.height = `${gridSize}px`;
       if (
         i === 0 && j < rows * .05 || i === cols && j < rows * .05 ||
         i === 0 && j > rows * .95 || i === cols && j > rows * .95 ||
@@ -23,7 +32,7 @@ function initializeGrid() {
       ) {
         pixel.className = 'border locked';
       } else {
-        pixel.className = 'pixel locked';
+        pixel.className = 'pixel';
         pixel.setAttribute('data-level', '0');
       }
       col.appendChild(pixel);
@@ -33,39 +42,48 @@ function initializeGrid() {
   }
 }
 
+// generate menu selection
 function initializeMenu() {
+  // app name
   const appNameDiv = document.createElement('div');
-  appNameDiv.innerHTML = 'Pixel&emsp14;Sketch';
-  appNameDiv.className = 'title';
+  appNameDiv.innerHTML = '<span class="active">Pixel</span><span' +
+    ' class="idle">Sketch</span>';
+  appNameDiv.className = 'locked';
   menu.appendChild(appNameDiv);
-
-  const separator = document.createElement('div');
-  separator.className = 'separator';
-  menu.appendChild(separator);
-
+  // divider
+  const divider = document.createElement('div');
+  divider.className = 'divider';
+  menu.appendChild(divider);
+  // draw button (toggles with erase button)
   const drawButtonDiv = document.createElement('div');
   drawButtonDiv.textContent = 'Draw';
   drawButtonDiv.id = 'draw';
   drawButtonDiv.className = 'active button locked';
   menu.appendChild(drawButtonDiv);
-
+  // erase button (toggles with draw button)
   const eraseButtonDiv = document.createElement('div');
   eraseButtonDiv.textContent = 'Erase';
   eraseButtonDiv.id = 'erase';
   eraseButtonDiv.className = 'idle button locked';
   menu.appendChild(eraseButtonDiv);
-
+  // pressure toggles three levels in events module (default is low)
   const pressureButtonDiv = document.createElement('div');
   pressureButtonDiv.textContent = 'Pressure: low';
   pressureButtonDiv.id = 'pressure';
   pressureButtonDiv.className = 'idle button locked';
   menu.appendChild(pressureButtonDiv);
-
+  // pixel size toggles three sizes (3x, 6x, 9x)
   const pixelSizeButtonDiv = document.createElement('div');
   pixelSizeButtonDiv.textContent = 'Pixels: 3x';
   pixelSizeButtonDiv.id = 'size';
   pixelSizeButtonDiv.className = 'idle button locked';
   menu.appendChild(pixelSizeButtonDiv);
+  // toggle between two css profiles
+  const invertButtonDiv = document.createElement('div');
+  invertButtonDiv.textContent = 'Invert';
+  invertButtonDiv.id = 'invert';
+  invertButtonDiv.className = 'idle button locked';
+  menu.appendChild(invertButtonDiv);
 }
 
 export function drawInterface() {
